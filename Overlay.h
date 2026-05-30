b9bc7a7f94930bb7702443d34f5b9cd02241bf62
wychengnb <441079214@qq.com>
2026-05-30 12:35:19 +0800

    更新 Overlay.h

--- Overlay.h
+++ Overlay.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "Common.h"
 @interface PubgOverlay : UIWindow
--(void)renderSkeleton:(Skeleton)sk mat:(float*)m;
+-(void)renderEnemies:(Enemy*)enemies count:(int)cnt matrix:(float*)m;
 -(void)clearAll;
 @end
