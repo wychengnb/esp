e3c8e07dbc8fe3d796e8bd22a18611b29fe37657
wychengnb <441079214@qq.com>
2026-05-30 12:26:50 +0800

    更新 Common.h

--- Common.h
+++ Common.h
@@ -2,8 +2,19 @@
 #define Common_h
 typedef struct{float x,y,z;}Vec3;
 typedef struct{
-    Vec3 head;Vec3 torso;Vec3 lhand,rhand;Vec3 lfoot,rfoot;
+    Vec3 head;
+    Vec3 torso;
+    Vec3 lhand,rhand;
+    Vec3 lfoot,rfoot;
 }Skeleton;
+typedef struct{
+    Skeleton sk;
+    float hp;
+    float maxhp;
+    float distance;
+    int is_visible; // 1=可见(不隔墙) 0=不可见(隔墙)
+}Enemy;
 #define PIPE_READ  0
 #define PIPE_WRITE 1
+#define MAX_ENEMY 16
 #endif
